9ced1475f6331c6a371920902c8b9c06b76989b4
Johannes <office@atelierbuchner.at>
2023-11-08 14:01:38 +0100

    charakter zindex fix

--- functions/calculateToolTipPosition.jsx
+++ functions/calculateToolTipPosition.jsx
@@ -1,4 +1,4 @@
 export default function calculateToolTipPosition(ballsPerTree, currentTree, id) {
     console.log((currentTree + 1) * ballsPerTree, id);
-    return (currentTree + 1) * ballsPerTree - id <= 44;
+    return (currentTree + 1) * ballsPerTree - id <= 21;
 }
